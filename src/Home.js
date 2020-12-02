be08b5367a56892fb590fd98db2fc1ec89412c16
Dominik Wilk <hiszpaniec@gmail.com>
2020-12-02 15:58:23 +0100

    Changed static text, and changed gitignore.

--- src/Home.js
+++ src/Home.js
@@ -2,5 +2,5 @@ import React from 'react';
 
 export default props => (<div>
     <h1>Hello {props.name}!</h1>
-    <button onClick={() => { props.clickHandler('Frontend name') }}>Zmiana</button>
+    <button onClick={() => { props.clickHandler('Frontend name') }}>Change in front-end side.</button>
 </div>);
